cd7c764ee3875389e5fba908cd86bbeea3c6f273
hadamelino <hadamuhammad@gmail.com>
2022-06-02 15:43:48 +0800

    Initial Commit

---
+++ BawaYa_DesignRevised WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  BawaYa_DesignRevised WatchKit Extension
+//
+//  Created by Hada Melino Muhammad on 02/06/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ BawaYa_DesignRevised/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BawaYa_DesignRevised
+//
+//  Created by Hada Melino Muhammad on 02/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
