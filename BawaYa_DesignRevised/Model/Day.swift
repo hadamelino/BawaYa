06d78d181540057e7a5caf16d2b9e5ddce8d61d4
hadamelino <hadamuhammad@gmail.com>
2022-06-07 11:43:14 +0700

    Add stackview and constraint for month and chevron to switch between months

---
+++ BawaYa_DesignRevised/Model/Day.swift
@@ -0,0 +1,14 @@
+//
+//  Day.swift
+//  BawaYa_DesignRevised
+//
+//  Created by Hada Melino Muhammad on 07/06/22.
+//
+
+import Foundation
+
+struct Day {
+    let date: Date
+    let number: String
+    let isSelected: Bool
+}
--- BawaYa_DesignRevised/StartViewController.swift
+++ BawaYa_DesignRevised/StartViewController.swift
@@ -17,11 +17,37 @@ class StartViewController: UIViewController {
         return theImageView
     }()
     
+    let monthLabel: UILabel = {
+        let theMonthLabel = UILabel()
+        theMonthLabel.translatesAutoresizingMaskIntoConstraints = false
+        theMonthLabel.font = UIFont(name: "PT Sans", size: 21)
+        theMonthLabel.text = "June 2022"
+        return theMonthLabel
+    }()
+    
+    let monthChevron: UIImageView = {
+        let chevron = UIImageView()
+        chevron.translatesAutoresizingMaskIntoConstraints = false
+        chevron.image = UIImage(systemName: "chevron.down")
+        chevron.tintColor = .black
+        return chevron
+    }()
+    
+    let monthStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .leading
+        //stackView.backgroundColor = .yellow
+        return stackView
+    }()
+    
     let label: UILabel = {
         let theLabel = UILabel()
         theLabel.translatesAutoresizingMaskIntoConstraints = false
         theLabel.numberOfLines = 2
-        theLabel.font = UIFont(name: "PT Sans-Bold", size: 21)
+        theLabel.font = UIFont(name: "PT Sans", size: 21)
         
         theLabel.textAlignment = .center
         theLabel.text = "It’s time to get reminded when you \nare going to some places"
@@ -52,6 +78,8 @@ class StartViewController: UIViewController {
         return collectionView
     }()
     
+    private let calendar = Calendar(identifier: .gregorian)
+    
 
     
     override func viewDidLoad() {
@@ -62,6 +90,7 @@ class StartViewController: UIViewController {
         setAutoLayoutImage()
         setAutoLayoutLabel()
         setAutoLayoutButton()
+        
         
     }
     
@@ -74,20 +103,39 @@ class StartViewController: UIViewController {
     @objc func addButtonPressed() {
         print("Add button Pressed!")
     }
-    
+        
     func setAutoLayoutCalendar() {
+        monthStack.addArrangedSubview(monthLabel)
+        monthStack.addArrangedSubview(monthChevron)
+        view.addSubview(monthStack)
         view.addSubview(calendarCollectionView)
-        calendarCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        monthStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        monthStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        monthStack.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3).isActive = true
+        monthStack.heightAnchor.constraint(equalTo: calendarCollectionView.heightAnchor, multiplier: 0.3).isActive = true
+        
+//        monthChevron.leadingAnchor.constraint(equalTo: monthLabel.trailingAnchor, constant: 5).isActive = true
+//        //monthChevron.topAnchor.constraint(equalTo: monthLabel.topAnchor).isActive = true
+//        //monthChevron.bottomAnchor.constraint(equalTo: monthLabel.bottomAnchor).isActive = true
+//        monthChevron.heightAnchor.constraint(equalTo: monthLabel.heightAnchor, multiplier: 0.5).isActive = true
+//
+//        monthLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+//        monthLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+//        monthLabel.widthAnchor.constraint(equalTo: calendarCollectionView.widthAnchor, multiplier: 0.25).isActive = true
+//        monthLabel.heightAnchor.constraint(equalTo: calendarCollectionView.heightAnchor, multiplier: 0.5).isActive = true
+
+        calendarCollectionView.topAnchor.constraint(equalTo: monthStack.bottomAnchor, constant: 10).isActive = true
         calendarCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
         calendarCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
-        calendarCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        calendarCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1).isActive = true
     }
     
     func setAutoLayoutImage() {
         view.addSubview(imageView)
         imageView.topAnchor.constraint(equalTo: calendarCollectionView.bottomAnchor, constant: 20).isActive = true
         imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4).isActive = true
-        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
@@ -111,3 +159,7 @@ class StartViewController: UIViewController {
 
 }
 
+private extension StartViewController {
+    
+    
+}
